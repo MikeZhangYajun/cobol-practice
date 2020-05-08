c5ac620cdf627aadf8e98bd53d1606c59b2d9979
Yajun Zhang <64558297+MikeZhangYajun@users.noreply.github.com>
2020-05-07 20:59:09 -0400

    Initial Commit

---
+++ HelloWorld.cbl
@@ -0,0 +1,16 @@
+       program-id. HelloWorld as "HelloWorld".
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+       01 my-message pic x(35) value
+           "Hello from Visual COBOL for Eclipse"
+       procedure division.
+           display my-message
+
+
+           goback.
+
+       end program HelloWorld.
