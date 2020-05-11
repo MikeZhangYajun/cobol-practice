033acda396c0d747714731384656a47f1b2eba23
Yajun Zhang <64558297+MikeZhangYajun@users.noreply.github.com>
2020-05-10 20:40:50 -0400

    changed the message

--- HelloWorld.cbl
+++ HelloWorld.cbl
@@ -6,7 +6,11 @@
        data division.
        working-storage section.
        01 my-message pic x(35) value
-           "Hello from Visual COBOL for Eclipse"
+           "Hello World from Visual COBOL for Eclipse"
+       
+           
+           
+           
        procedure division.
            display my-message
 
